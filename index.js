45a10f31e7d1fbffb7e9a8979eb9c2f65717d848
tubisaffo <tubisaffo@gmail.com>
2024-03-16 16:12:26 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+
+console.log(introduction("Saffo"));
+
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
+
+console.log(introductionWithLanguage("Aki", "Ember.js"));
+console.log(introductionWithLanguage("Samip", "React"));
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Gracie", "Python"));
